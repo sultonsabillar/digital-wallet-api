529e5f8975b8fca848451b614de9eeea435390be
Sulton Sabilla Rosad <sultonsabillar@gmail.com>
2025-05-10 23:58:13 +0700

    feat : menambahkan deposit

--- src/services/server.ts
+++ src/services/server.ts
@@ -1,12 +1,15 @@
 import express from 'express';
-import accountRoutes from '../routes/accountRoutes';
+import accountRoutes from '../routes/accountRoutes';  // Rute untuk akun
+import depositRoutes from '../routes/depositRoutes';  // Rute untuk transaksi deposit
 
 const app = express();
 const PORT = 3000;
 
-app.use(express.json());
+app.use(express.json());  
 
 app.use('/api/accounts', accountRoutes);
+
+app.use('/api/accounts', depositRoutes); 
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
